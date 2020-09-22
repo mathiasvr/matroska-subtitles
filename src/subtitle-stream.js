ae304aefa3d4b5ea3980c8d0c4fd1ab57d5fc6ba
Mathias Rasmussen <mathiasvr@gmail.com>
2020-09-22 22:44:19 +0200

    fixes

--- src/subtitle-stream.js
+++ src/subtitle-stream.js
@@ -27,11 +27,12 @@ export class SubtitleStream extends SubtitleParserBase {
     if (this.unstable) {
       // the ebml decoder expects to see a tag, so we won't use it until we find a cluster
       for (let i = 0; i < chunk.length - 12; i++) {
+        // cluster id
         if (chunk[i] === 0x1f && chunk[i + 1] === 0x43 && chunk[i + 2] === 0xb6 && chunk[i + 3] === 0x75) {
           // length of cluster size tag
           const len = 8 - Math.floor(Math.log2(chunk[i + 4]))
           // first tag in cluster is cluster timecode
-          if (len < 9 && chunk[i + 4 + len] === 0xe7) {
+          if (chunk[i + 4 + len] === 0xe7) {
             // okay this is probably a cluster
             this.unstable = false
             this.decoder.write(chunk.slice(i))
