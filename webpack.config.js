18146986540c98aeba6d3f2a4ef0a5b4cfd306cb
Mathias Rasmussen <mathiasvr@gmail.com>
2021-09-20 21:50:20 +0200

    Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
+  mode: 'production',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
